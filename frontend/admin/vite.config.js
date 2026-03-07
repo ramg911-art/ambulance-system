920931b044eab490f6d5b56e33a8f8387b3914f4
Ram Gopal <ramg911@gmail.com>
2026-03-07 12:28:29 +0530

    upgrade majord

--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    include: ['jspdf'],
+  },
   server: {
     port: 5176,
     proxy: {
